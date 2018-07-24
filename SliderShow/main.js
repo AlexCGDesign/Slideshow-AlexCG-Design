12547ed256cc98f3badc9cec894ee02eea1ee20d
Jordan Alexander Cruz <37056514+AlexCGDesign@users.noreply.github.com>
2018-07-24 17:47:29 -0500

    Add files via upload

---
+++ SliderShow/main.js
@@ -0,0 +1,35 @@
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n){
+    muestraSlides( indice+=n );
+}
+
+function posicionSlide(n){
+    muestraSlides(indice=n);
+}
+setInterval(function tiempo(){
+    muestraSlides(indice+=1)
+},4000);
+function muestraSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('miSlider');
+    let barras = document.getElementsByClassName('barra');
+
+    if(n > slides.length){
+        indice = 1;
+    }
+    if(n < 1){
+        indice = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < barras.length; i++){
+        barras[i].className = barras[i].className.replace(" active", "");
+    }
+
+    slides[indice-1].style.display = 'block';
+    barras[indice-1].className += ' active';
+
+}
